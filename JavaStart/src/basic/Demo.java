da6b4d33e849805e6fbc74061563b95ffbc8e74a
MOH RAMJAN <ramjangori6666@gmail.com>
2025-12-26 21:57:26 +0530

    Starting with basics of java

---
+++ JavaStart/src/basic/Demo.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class Demo {
+   public static void main(String srg[]) {
+	   
+	   // Demo -> Class Name 
+	   // public static void main -> main method inside class 
+	   
+	   
+	   System.out.println("Hello Ramjan"); // -> Use to print something on Console Screen 
+	   
+	   /* 
+	     System ->System is a Class Name which is inside Java.lang package 
+	     out -> is s static variable of System class of type printStream
+	     println -> is a method inside PrintStream Class
+	     
+	     */
+   }
+   
+   
+}
